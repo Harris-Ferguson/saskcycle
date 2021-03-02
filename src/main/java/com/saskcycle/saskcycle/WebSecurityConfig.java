f9005ffc42e3b725b5c1ceae10461289592d6c7c
Harris <haf094@usask.ca>
2021-03-02 15:44:11 -0600

    added user auth with one login

---
+++ src/main/java/com/saskcycle/saskcycle/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.saskcycle.saskcycle;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                //first specify pages needing no permissions
+                .antMatchers("/", "/home").permitAll().anyRequest().authenticated()
+                .and()
+                //specify the login page with login form
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        // Here we need to return the DB user Details Service
+        // for now, we simply make a single default authorize user
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("pass")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}

e64c4d18984082e68e2ef9116437a0f83b6f4361
Harris <haf094@usask.ca>
2021-03-06 17:58:39 -0600

    added user authentication form connected to spring security

---
+++ src/main/java/com/saskcycle/saskcycle/security/CustomRequestCache.java
@@ -0,0 +1,21 @@
+package com.saskcycle.saskcycle.security;
+
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This class Caches any requests to secure pages while unauthenticated, so we can redirect them to
+ * where they were trying to go after they log in
+ */
+public class CustomRequestCache extends HttpSessionRequestCache {
+
+	@Override
+	public void saveRequest(HttpServletRequest request, HttpServletResponse response) { 
+		if (!SecurityUtils.isFrameworkInternalRequest(request)) {
+			super.saveRequest(request, response);
+		}
+	}
+
+}

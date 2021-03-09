b1841314e91950e47287b987ae30edd8ffd9d22f
norahridley <nar174@mail.usask.ca>
2021-03-09 15:46:27 -0600

    Created skeleton UI for the results page

--- src/main/java/com/saskcycle/saskcycle/view/SearchResultsView.java
+++ src/main/java/com/saskcycle/saskcycle/view/SearchResultsView.java
@@ -1,9 +1,14 @@
 package com.saskcycle.saskcycle.view;
 
+import com.vaadin.flow.component.checkbox.CheckboxGroup;
+import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
 
 @Route(value = "results", layout = SearchResultsLayout.class)
 @PageTitle("SaskCycle | Results")
@@ -11,6 +16,34 @@ public class SearchResultsView  extends VerticalLayout {
 
     public SearchResultsView() {
 
-        add(new H1("results"));
+        VerticalLayout filterGroup = new VerticalLayout();
+
+        Select<String> useSelect = new Select<>();
+        useSelect.setItems("Searching", "Giving");
+        useSelect.setLabel("What do you want to do?");
+
+        Select<String> sortSelect = new Select<>();
+        sortSelect.setItems("Most recent", "Least recent");
+        sortSelect.setLabel("Sort by");
+
+        CheckboxGroup<String> includeGroup = new CheckboxGroup<>();
+        includeGroup.setLabel("Show results for");
+        includeGroup.setItems("Appliances", "Clothing", "Electronics", "Furniture");
+        includeGroup.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
+
+        CheckboxGroup<String> excludeGroup = new CheckboxGroup<>();
+        excludeGroup.setLabel("Hide results for");
+        excludeGroup.setItems("Appliances", "Clothing", "Electronics", "Furniture");
+        excludeGroup.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
+
+
+        filterGroup.add(useSelect, sortSelect, includeGroup, excludeGroup);
+
+        HorizontalLayout resultsGroup = new HorizontalLayout();
+        resultsGroup.add(filterGroup);
+
+
+
+        add(new H1("All listings"), resultsGroup);
     }
 }

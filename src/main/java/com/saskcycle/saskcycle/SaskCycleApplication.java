c977f09948d897ed8526ba4bb0f86a254c9aa80a
Declan <djh723@usask.ca>
2021-03-08 19:33:25 -0600

    Added fake runtime test

--- src/main/java/com/saskcycle/saskcycle/SaskCycleApplication.java
+++ src/main/java/com/saskcycle/saskcycle/SaskCycleApplication.java
@@ -1,31 +1,62 @@
 package com.saskcycle.saskcycle;
 
 
-import com.mongodb.ClientSessionOptions;
-import com.mongodb.client.*;
-import com.mongodb.connection.ClusterDescription;
-import org.bson.Document;
-import org.bson.conversions.Bson;
+import com.saskcycle.model.Account;
+import com.saskcycle.model.authorities.AccountAuthority;
+import com.saskcycle.repo.UserAccountRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.core.GrantedAuthority;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 @SpringBootApplication
-public class SaskCycleApplication {
+@ComponentScan(basePackages = {"com.saskcycle"})
+@EntityScan("com.saskcycle.model")
+@EnableMongoRepositories(basePackageClasses = UserAccountRepo.class)
+public class SaskCycleApplication implements CommandLineRunner {
+
 
 //	private MongoRepository repository;
 
+	@Autowired
+	private UserAccountRepo UAR;
+
+
+
 	public static void main(String[] args) {
-
 		SpringApplication.run(SaskCycleApplication.class, args);
-
-//		Mongo
 
 	}
 
-//	public void run(String...args) throws Exception{
-//		repository.
-//	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hi");
+		System.out.println(count());
+
+		ArrayList<GrantedAuthority> authorities = new ArrayList<>();
+
+		AccountAuthority auth = new AccountAuthority();
+
+		authorities.add(auth);
+
+
+		Account newAccount = new Account("Harris add", "The Big Password",
+				authorities,"101", "FakeEmail@123.com");
+		UAR.insert(newAccount);
+//		System.out.println(UAR.toString());
+
+	}
+
+	public int count(){
+		return (int) UAR.count();
+	}
 }

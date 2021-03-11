52a2d9d356c3d3068fee26a8de6b449681fef767
norahridley <nar174@mail.usask.ca>
2021-03-11 17:22:57 -0600

    Set up a primitive results page to display all listings

--- src/main/java/com/saskcycle/saskcycle/view/SearchResultsView.java
+++ src/main/java/com/saskcycle/saskcycle/view/SearchResultsView.java
@@ -1,22 +1,35 @@
 package com.saskcycle.saskcycle.view;
 
+import com.saskcycle.controller.SearchController;
+import com.saskcycle.model.Post;
 import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 
 @Route(value = "results", layout = SearchResultsLayout.class)
 @PageTitle("SaskCycle | Results")
 public class SearchResultsView  extends VerticalLayout {
 
-    public SearchResultsView() {
+    @Autowired
+    private SearchController SC;
+
+    @PostConstruct
+    public void init(){
 
         VerticalLayout filterGroup = new VerticalLayout();
+        filterGroup.setWidth("200px");
 
         Select<String> useSelect = new Select<>();
         useSelect.setItems("Searching", "Giving");
@@ -41,11 +54,41 @@ public class SearchResultsView  extends VerticalLayout {
         excludeGroup.setItems("Appliances", "Clothing", "Electronics", "Furniture");
         excludeGroup.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
 
+        List<Post> posts = SC.getAllGivingAway();
+
+        Scroller scroller = new Scroller();
+        scroller.setHeight("200px");
+        scroller.setWidth("100%");
+
+        VerticalLayout content= new VerticalLayout();
+
+        content.setAlignItems(Alignment.STRETCH);
+        content.setWidth("100%");
+        content.setSpacing(false);
+
+        for (int i=0; i < posts.size(); i++) {
+            VerticalLayout component = new VerticalLayout();
+            component.add(new H3(posts.get(i).title));
+            component.add(new H5(posts.get(i).location + " away"));
+            component.add(new H4(posts.get(i).description));
+            component.add(String.valueOf(posts.get(i).tags));
+            content.add(component);
+            component.getStyle().set("border", "1px solid #9E9E9E");
+        }
+
+        //scroller.setContent(content);
+
 
         filterGroup.add(useSelect, sortSelect, postChoice, includeGroup, excludeGroup);
 
         HorizontalLayout resultsGroup = new HorizontalLayout();
-        resultsGroup.add(filterGroup);
+        resultsGroup.setAlignItems(Alignment.START);
+        resultsGroup.setWidth("100%");
+        resultsGroup.setSpacing(false);
+        resultsGroup.add(filterGroup, content);
+
+
+
 
 
 

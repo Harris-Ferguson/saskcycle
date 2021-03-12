1952b76eae85113054b45c1c5abb198e634ec865
Declan <djh723@usask.ca>
2021-03-12 10:28:25 -0600

    Resubmitted Controller methods

--- src/main/java/com/saskcycle/controller/SearchController.java
+++ src/main/java/com/saskcycle/controller/SearchController.java
@@ -22,12 +22,6 @@ public class SearchController {
 
     /* --------- Attributes --------- */
 
-//    @Autowired
-//    private PostsRepo PR;
-//
-//    @Autowired
-//    private BusinessesPostsRepo BR;
-
 
     @Autowired
     private PostsDAOInterface Paccess;
@@ -36,21 +30,17 @@ public class SearchController {
     private BusinessDAOInterface Baccess;
 
 
-    public SearchController(){
-    }
+    public SearchController(){}
 
 
     /* ---------  Methods  --------- */
 
-//    public BusinessDAO getBaccess() {
-//        return Baccess;
-//    }
-//
-//    public PostsDAO getPaccess() {
-//        return Paccess;
-//    }
 
-    public List<Post> getAllGivingAway(){
+    /***
+     * Gets every post - giving away, getting, and static business posts - in one query
+     * @return A lLst of Post and Business items (which can be displayed the same way).
+     */
+    public List<Post> getAllListings(){
         List<Post> allPosts = new ArrayList<>();
         allPosts.addAll(Paccess.AllPosts());
         allPosts.addAll(Baccess.AllPosts());
@@ -58,19 +48,93 @@ public class SearchController {
         return allPosts;
         }
 
-//
-//    public static void main(String[] args) {
-//        SearchController controller = new SearchController();
-//        List<Post> allPost = controller.getAllGivingAway();
-//
-//        for (Post p : allPost){
-//            System.out.println(p.description);
-//        }
-//    }
+
+
+    /***
+     * Gets all giving away and getting posts.
+     * @return a List of Posts
+     */
+    public List<Post> getAllPosts(){
+        return new ArrayList<>(Paccess.AllPosts());
+    }
+
+
+    /***
+     * Adds a post to the SaskCycle system
+     * @param post: A post object
+     */
+    public void addPost(Post post){
+        Paccess.addPost(post);
+    }
 
 
 
+
+
+    /***
+     * Gets all Listings that contain a certain tag
+     * @param tag: a tag of a search item type
+     * @return a list of every post in the database containing that tag
+     */
+    public  List<Post> getAllListingsByTag(String tag) {
+        List<Post> postsByTag = new ArrayList<>();
+        List<Post> allPosts = getAllListings();
+        for (Post p : allPosts) {
+            if (p.tags.contains(tag)) postsByTag.add(p);
+        }
+        return postsByTag;
+    }
+
+    /***
+     * Gets all Posts that contain a certain tag
+     * @param tag: the tag of a search item type
+     * @return a list of every post in the database containing that tag
+     */
+    public  List<Post> getAllPostsByTag(String tag) {
+        List<Post> postsByTag = new ArrayList<>();
+        List<Post> allPosts = getAllPosts();
+        for (Post p : allPosts) {
+            if (p.tags.contains(tag)) postsByTag.add(p);
+        }
+        return postsByTag;
+    }
+
+    /***
+     * Returns post that has ID marker ID
+     * @param id: a String containing the identification marker of a post
+     * @return a post's ID
+     */
+    public Post getPostByID(String id){
+        return Paccess.searchByID(id);
+    }
+
+    /***
+     * Returns business that has ID marker ID
+     * @param id: a String containing the identification marker of a business
+     * @return a business's ID
+     */
+    public Business getBusinessByID(String id){
+        return Baccess.searchByID(id);
+    }
+
+    /***
+     * Gets all posts containing a keyword in the description or title
+     * @param keyword: A string the user wishes to search by
+     * @return a list of posts containing the keyphrase specified by the searcher
+     */
+    public List<Post> getAllPostsByKeyword(String keyword){
+        List<Post> filteredPosts = new ArrayList<>();
+        for (Post p : getAllPosts()){
+            // Checks if case insensitive keyword is in title or description
+            if (p.title.toLowerCase().contains(keyword.toLowerCase()) ||
+                    p.description.toLowerCase().contains(keyword.toLowerCase()))
+                filteredPosts.add(p);
+        }
+        return filteredPosts;
+    }
 
 
 
 }
+
+

c27722f3113f4223926deefb6312e9b771077a59
Harris <haf094@usask.ca>
2021-03-16 00:02:25 -0600

    added checking for places and placing markers

--- frontend/src/js/map-script.js
+++ frontend/src/js/map-script.js
@@ -1,17 +1,48 @@
 // Create the script tag, set the appropriate attributes
 var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBBlDTjB1qNQkH_h7fVb4W9XNeEjfvJLEU&callback=initMap&libraries=&v=weekly';
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBBlDTjB1qNQkH_h7fVb4W9XNeEjfvJLEU&callback=initMap&libraries=places&v=weekly';
 script.async = true;
+
+var map;
+var infowindow;
+var servce;
 
 // Attach your callback function to the `window` object
 window.initMap = function() {
-    let map;
+    var saskatoon = new google.maps.LatLng(52.118, -106.643)
+
+    infowindow = new google.maps.InfoWindow();
 
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 52.1187838, lng: -106.634 },
-        zoom: 12,
+        center: saskatoon,
+        zoom: 10,
+    });
+
+    var request = {
+        query : 'Recycle',
+        fields : ['name', 'geometry']
+    };
+
+    var service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i], results[i].name);
+                console.log(results[i]);
+            }
+            map.setCenter(results[0].geometry.location);
+        }
     });
 };
+
+function createMarker(place, name) {
+    new google.maps.Marker({
+        position: place.geometry.location,
+        title: name,
+        map: map
+    });
+}
 
 // Append the 'script' element to 'head'
 document.head.appendChild(script);

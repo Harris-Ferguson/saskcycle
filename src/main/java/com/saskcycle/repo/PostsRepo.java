79d07f1e6ee269d0f0901d7151c2844f2dcede94
Declan <djh723@usask.ca>
2021-03-16 00:43:30 -0600

    Uncommented and left explanatory note for unused PostRepo methods

--- src/main/java/com/saskcycle/repo/PostsRepo.java
+++ src/main/java/com/saskcycle/repo/PostsRepo.java
@@ -4,17 +4,22 @@ import com.saskcycle.model.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface PostsRepo extends MongoRepository<Post, String> {
-  //
-  //    Post findByTag(String tag);
-  //
-  //    Post findByLocation(String location, String tag);
-  //
-  //    Post findByLocationAndTag(String location, String tag);
-  //
-  //    Post findByDateAndTag(Date date, String tag);
-  //
-  //    Post findByDate(Date date);
+
+    /* These methods may be used to improve the efficiency of database interactions,
+    * but currently are not operational. Possibly usable for a future version of SaskCycle*/
+
+      Post findByTag(String tag);
+
+      Post findByLocation(String location, String tag);
+
+      Post findByLocationAndTag(String location, String tag);
+
+      Post findByDateAndTag(Date date, String tag);
+
+      Post findByDate(Date date);
 
 }

60c4c4e68574baa7f3ef816b5eae4270b8b84fa3
Declan <djh723@usask.ca>
2021-03-16 01:09:32 -0600

    Recommented and left explanatory note for unused PostRepo methods, added input correctness checking for SearchController methods, turned application off of production mode

--- src/main/java/com/saskcycle/repo/PostsRepo.java
+++ src/main/java/com/saskcycle/repo/PostsRepo.java
@@ -6,20 +6,22 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 
+
 @Repository
 public interface PostsRepo extends MongoRepository<Post, String> {
 
-    /* These methods may be used to improve the efficiency of database interactions,
-    * but currently are not operational. Possibly usable for a future version of SaskCycle*/
+    /* These methods may be used to improve the efficiency of database interactions in PostDAO,
+    * but currently are not operational. Possibly usable for a future version of SaskCycle.
+    * Commented out to allow SaskCycle to run. */
 
-      Post findByTag(String tag);
-
-      Post findByLocation(String location, String tag);
-
-      Post findByLocationAndTag(String location, String tag);
-
-      Post findByDateAndTag(Date date, String tag);
-
-      Post findByDate(Date date);
+//      Post findByTag(String tag);
+//
+//      Post findByLocation(String location, String tag);
+//
+//      Post findByLocationAndTag(String location, String tag);
+//
+//      Post findByDateAndTag(Date date, String tag);
+//
+//      Post findByDate(Date date);
 
 }

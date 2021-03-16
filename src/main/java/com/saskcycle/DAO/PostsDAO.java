971b3ad266f93c458febd10d964f3c1f04b5c6c3
Declan <djh723@usask.ca>
2021-03-16 01:40:31 -0600

    Added headers and comments to DAO, finished cleaning code from project Alpha.

--- src/main/java/com/saskcycle/DAO/PostsDAO.java
+++ src/main/java/com/saskcycle/DAO/PostsDAO.java
@@ -23,6 +23,7 @@ public class PostsDAO implements PostsDAOInterface {
 
   @Override
   public List<Post> AllPosts() {
+    // Returns all posts from the Posts MongoDB database
     return PR.findAll();
   }
 
@@ -63,11 +64,13 @@ public class PostsDAO implements PostsDAOInterface {
 
   @Override
   public Post addPost(Post post) {
-    return PR.insert(post);
+  // Inserts a post to the Posts MongoDB Database
+  return PR.insert(post);
   }
 
   @Override
   public void deletePost(Post post) {
+    // Deletes a post from the Posts MongoDB Database
     PR.delete(post);
   }
 

82a37757808c668c2eadd339c23196c7ffb3c4f1
norahridley <nar174@mail.usask.ca>
2021-03-20 11:13:43 -0600

    Rough dialog window pops up when user clicks on an event in the calendar with event info

---
+++ src/main/java/com/saskcycle/saskcycle/view/uiViews/DemoDialog.java
@@ -0,0 +1,211 @@
+//package com.saskcycle.saskcycle.view.uiViews;
+//
+//import com.vaadin.flow.component.button.Button;
+//import com.vaadin.flow.component.button.ButtonVariant;
+//import com.vaadin.flow.component.checkbox.Checkbox;
+//import com.vaadin.flow.component.checkbox.CheckboxGroup;
+//import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
+//import com.vaadin.flow.component.combobox.ComboBox;
+//import com.vaadin.flow.component.customfield.CustomField;
+//import com.vaadin.flow.component.datepicker.DatePicker;
+//import com.vaadin.flow.component.dialog.Dialog;
+//import com.vaadin.flow.component.html.Span;
+//import com.vaadin.flow.component.orderedlayout.FlexComponent;
+//import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+//import com.vaadin.flow.component.textfield.TextArea;
+//import com.vaadin.flow.component.textfield.TextField;
+//import com.vaadin.flow.component.timepicker.TimePicker;
+//import com.vaadin.flow.data.binder.Binder;
+//
+//import org.apache.catalina.loader.ResourceEntry;
+//import org.vaadin.stefan.fullcalendar.Entry;
+//import org.vaadin.stefan.fullcalendar.FullCalendar;
+//import org.vaadin.stefan.fullcalendar.Timezone;
+//
+//import java.time.*;
+//import java.util.Set;
+//
+//public class DemoDialog extends Dialog {
+//    private static final long serialVersionUID = 1L;
+//
+//    private static final String[] COLORS = {"tomato", "orange", "dodgerblue", "mediumseagreen", "gray", "slateblue", "violet"};
+//    private final DialogEntry dialogEntry;
+//    private final CustomDateTimePicker fieldStart;
+//    private final CustomDateTimePicker fieldEnd;
+//    private final CheckboxGroup<DayOfWeek> fieldRDays;
+//
+//    public DemoDialog(FullCalendar calendar, ResourceEntry entry, boolean newInstance) {
+//        this.dialogEntry = DialogEntry.of(entry, calendar.getTimezone());
+//
+//        setCloseOnEsc(true);
+//        setCloseOnOutsideClick(true);
+//
+//        setWidth("500px");
+//
+//        // init field
+//
+//        // init binder
+//
+//        Binder<DialogEntry> binder = new Binder<>(DialogEntry.class);
+//
+//        // required fields
+//        binder.forField(fieldTitle).asRequired().bind(DialogEntry::getTitle, DialogEntry::setTitle);
+//        binder.forField(fieldStart).asRequired().bind(DialogEntry::getStart, DialogEntry::setStart);
+//        binder.forField(fieldEnd).asRequired().bind(DialogEntry::getEnd, DialogEntry::setEnd);
+//
+//        // optional fields
+//        binder.bind(fieldColor, DialogEntry::getColor, DialogEntry::setColor);
+//        binder.bind(fieldDescription, DialogEntry::getDescription, DialogEntry::setDescription);
+//        binder.bind(fieldAllDay, DialogEntry::isAllDay, DialogEntry::setAllDay);
+//        binder.bind(fieldRecurring, DialogEntry::isRecurring, DialogEntry::setRecurring);
+//        binder.bind(fieldRDays, DialogEntry::getRecurringDays, DialogEntry::setRecurringDays);
+//
+//        binder.setBean(dialogEntry);
+//
+//
+//
+//        VerticalLayout mainLayout = new VerticalLayout();
+//
+//        mainLayout.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.STRETCH);
+//        mainLayout.setSizeFull();
+//
+//        mainLayout.getElement().getStyle().set("overflow-y", "auto");
+//
+//        add(mainLayout);
+//
+//    }
+//
+//    protected void updateRecurringFieldsState(boolean recurring) {
+//        if (recurring) {
+//            fieldStart.setLabel("Start of recurrence");
+//            fieldEnd.setLabel("End of recurrence");
+//        } else {
+//            fieldStart.setLabel("Start");
+//            fieldEnd.setLabel("End");
+//        }
+//        fieldRDays.setVisible(recurring);
+//    }
+//
+//    /**
+//     * see https://vaadin.com/components/vaadin-custom-field/java-examples
+//     */
+//    public static class CustomDateTimePicker extends CustomField<LocalDateTime> {
+//
+//        private final DatePicker datePicker = new DatePicker();
+//        private final TimePicker timePicker = new TimePicker();
+//        private boolean dateOnly;
+//
+//        CustomDateTimePicker(String label) {
+//            setLabel(label);
+//            add(datePicker, timePicker);
+//        }
+//
+//        @Override
+//        protected LocalDateTime generateModelValue() {
+//            final LocalDate date = datePicker.getValue();
+//            final LocalTime time = timePicker.getValue();
+//
+//            if (date != null) {
+//                if (dateOnly || time == null) {
+//                    return date.atStartOfDay();
+//                }
+//
+//                return LocalDateTime.of(date, time);
+//            }
+//
+//            return null;
+//
+//        }
+//
+//        @Override
+//        protected void setPresentationValue(
+//                LocalDateTime newPresentationValue) {
+//            datePicker.setValue(newPresentationValue != null ? newPresentationValue.toLocalDate() : null);
+//            timePicker.setValue(newPresentationValue != null ? newPresentationValue.toLocalTime() : null);
+//        }
+//
+//        public void setDateOnly(boolean dateOnly) {
+//            this.dateOnly = dateOnly;
+//            timePicker.setVisible(!dateOnly);
+//        }
+//    }
+//
+//    private static class DialogEntry {
+//        private String id;
+//        private String title;
+//        private String color;
+//        private String description;
+//        private LocalDateTime start;
+//        private LocalDateTime end;
+//        private boolean allDay;
+//        private boolean recurring;
+//        private Set<DayOfWeek> recurringDays;
+//        private Timezone timezone;
+//        private ResourceEntry entry;
+//
+//        public static DialogEntry of(ResourceEntry entry, Timezone timezone) {
+//            DialogEntry dialogEntry = new DialogEntry();
+//
+//            dialogEntry.setTimezone(timezone);
+//            dialogEntry.setEntry(entry);
+//
+//            dialogEntry.setTitle(entry.getTitle());
+//            dialogEntry.setColor(entry.getColor());
+//            dialogEntry.setDescription(entry.getDescription());
+//            dialogEntry.setAllDay(entry.isAllDay());
+//
+//            boolean recurring = entry.isRecurring();
+//            dialogEntry.setRecurring(recurring);
+//
+//            if (recurring) {
+//                dialogEntry.setRecurringDays(entry.getRecurringDaysOfWeeks());
+//
+//                dialogEntry.setStart(entry.getRecurringStartDate(timezone).atTime(entry.getRecurringStartTime()));
+//                dialogEntry.setEnd(entry.getRecurringEndDate(timezone).atTime(entry.getRecurringEndTime()));
+//            } else {
+//                dialogEntry.setStart(entry.getStart(timezone));
+//                dialogEntry.setEnd(entry.getEnd(timezone));
+//            }
+//
+//            return dialogEntry;
+//        }
+//
+//        /**
+//         * Updates the stored entry instance and returns it after updating.
+//         *
+//         * @return entry instnace
+//         */
+//        private Entry updateEntry() {
+//            entry.setTitle(title);
+//            entry.setColor(color);
+//            entry.setDescription(description);
+//            entry.setAllDay(allDay);
+//            entry.setRecurring(recurring);
+//
+//            if (recurring) {
+//                entry.setRecurringDaysOfWeeks(getRecurringDays());
+//
+//                entry.setStart((Instant) null);
+//                entry.setEnd((Instant) null);
+//
+//                entry.setRecurringStartDate(start.toLocalDate(), timezone);
+//                entry.setRecurringStartTime(allDay ? null : start.toLocalTime());
+//
+//                entry.setRecurringEndDate(end.toLocalDate(), timezone);
+//                entry.setRecurringEndTime(allDay ? null : end.toLocalTime());
+//            } else {
+//                entry.setStart(start, timezone);
+//                entry.setEnd(end, timezone);
+//
+//                entry.setRecurringStartDate(null);
+//                entry.setRecurringStartTime(null);
+//                entry.setRecurringEndDate(null);
+//                entry.setRecurringEndTime(null);
+//                entry.setRecurringDaysOfWeeks(null);
+//            }
+//
+//            return entry;
+//        }
+//    }
+//}
--- src/main/java/com/saskcycle/saskcycle/view/uiViews/EventView.java
+++ src/main/java/com/saskcycle/saskcycle/view/uiViews/EventView.java
@@ -6,7 +6,10 @@ import com.saskcycle.model.Event;
 import com.saskcycle.saskcycle.view.layouts.EventLayout;
 import com.vaadin.flow.component.charts.model.VerticalAlign;
 import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.H5;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -16,6 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.stefan.fullcalendar.*;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
+
+
+import org.apache.catalina.loader.ResourceEntry;
 
 
 import javax.annotation.PostConstruct;
@@ -47,7 +53,11 @@ public class EventView extends VerticalLayout {
     add(createToolBar(), createCalendar());
   }
 
-  public VerticalLayout createCalendar() {
+  /**
+   * Creates and styles calendar component and adds the events
+   * @return vertical layour containing a full calendar component
+   */
+  private VerticalLayout createCalendar() {
 
     VerticalLayout calLayout = new VerticalLayout();
 
@@ -57,12 +67,37 @@ public class EventView extends VerticalLayout {
     calLayout.add(calendar);
     calLayout.setFlexGrow(1, calendar);
 
+    calendar.addEntryClickedListener(event -> {
+      H3 eventTitle = new H3(event.getEntry().getTitle());
+      Dialog eventInfo =  new Dialog(eventTitle);
+
+      H5 startTime = new H5(String.valueOf(event.getEntry().getStart(calendar.getTimezone())));
+      H5 endTime = new H5(String.valueOf(event.getEntry().getEnd(calendar.getTimezone())));
+
+
+
+
+
+      eventInfo.add(startTime, endTime);
+
+
+
+      eventInfo.setHeight("400px");
+      eventInfo.setWidth("400px");
+
+      eventInfo.open();
+    });
+
     addEvents();
 
 
     return calLayout;
   }
 
+  /**
+   * Sets up the tool bar which allows the user to toggle between months and return to their current day
+   * @return vertical layout containing current month, toggle and home buttons
+   */
   private VerticalLayout createToolBar() {
 
     HorizontalLayout toolbar = new HorizontalLayout();
@@ -112,16 +147,21 @@ public class EventView extends VerticalLayout {
 
   }
 
+  /**
+   * Adds the events in the database to the calendar
+   */
   private void addEvents() {
 
     for (Event e : events) {
 
       Entry entry = new Entry();
       entry.setTitle(e.title);
+      // start info array format [month, day, start hour, start minute]
       entry.setStart(LocalDate.now().withMonth(e.startTime[0]).withDayOfMonth(e.startTime[1]).atTime(e.startTime[2], e.startTime[3]), calendar.getTimezone());
+      // end info array format [month, day, end hour, end minute]
       entry.setEnd(LocalDate.now().withMonth(e.endTime[0]).withDayOfMonth(e.endTime[1]).atTime(e.endTime[2], e.endTime[3]), calendar.getTimezone());
 
-      entry.setColor("#ff3333");
+      entry.setColor("#04584a");
 
       calendar.addEntry(entry);
     }

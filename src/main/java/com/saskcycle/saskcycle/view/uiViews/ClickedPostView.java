68ae217bd82d1336ce272ad1f3f36478c6f89987
lkp821 <Earthbound1>
2021-03-22 16:56:43 -0600

    have ability to see posts, and add to wishlist

--- src/main/java/com/saskcycle/saskcycle/view/uiViews/ClickedPostView.java
+++ src/main/java/com/saskcycle/saskcycle/view/uiViews/ClickedPostView.java
@@ -22,7 +22,7 @@ import com.vaadin.flow.router.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "clickedPost", layout = ClickedPostLayout.class)
-public class ClickedPostView extends VerticalLayout /*implements HasUrlParameter<String>, AfterNavigationObserver*/ {
+public class ClickedPostView extends VerticalLayout implements HasUrlParameter<String>, AfterNavigationObserver {
 
     private H1 title;
 
@@ -43,8 +43,7 @@ public class ClickedPostView extends VerticalLayout /*implements HasUrlParameter
     public ClickedPostView() {
 
         title = new H1("Placeholder post title");
-        Paragraph paragraph = new Paragraph("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
-
+        paragraph = new Paragraph();
         Button wishlistButton = new Button("Add to wishlist", new Icon(VaadinIcon.STAR));
         wishlistButton.addClickListener(e -> {
                 if (SecurityUtils.isUserLoggedIn()){
@@ -73,14 +72,18 @@ public class ClickedPostView extends VerticalLayout /*implements HasUrlParameter
     }
 
     // Methods that may work eventually in creating a custom post based on what's clicked
-//    @Override
-//    public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
-//        heading.setText(title);
-//
-//    }
-//
-//    @Override
-//    public void setParameter(BeforeEvent beforeEvent, String s) {
-//        title = s;
-//    }
+    @Override
+    public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
+        post = SC.getPostByID(id);
+        text = post.title;
+        title.setText(text);
+        paragraph.setText(post.description);
+
+    }
+
+    @Override
+    public void setParameter(BeforeEvent beforeEvent, String s) {
+        id = s;
+
+    }
 }

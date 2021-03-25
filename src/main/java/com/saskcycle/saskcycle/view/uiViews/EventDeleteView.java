1e9688220688a50443e34add1ff3b302d1132f1a
norahridley <nar174@mail.usask.ca>
2021-03-25 09:59:42 -0600

    Successfully display the posts created by the org account on my events display. To add preview and delete functionality.

--- src/main/java/com/saskcycle/saskcycle/view/uiViews/EventDeleteView.java
+++ src/main/java/com/saskcycle/saskcycle/view/uiViews/EventDeleteView.java
@@ -1,25 +1,40 @@
 package com.saskcycle.saskcycle.view.uiViews;
 
+import com.saskcycle.DAO.CurrentUserDAOInterface;
+import com.saskcycle.model.Post;
+import com.saskcycle.saskcycle.view.components.PostComponent;
 import com.saskcycle.saskcycle.view.layouts.PostCreateLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+
+import javax.annotation.PostConstruct;
 
 @Route(value = "delete-event", layout = PostCreateLayout.class)
 @PageTitle("SaskCycle | Event Create")
 @Secured("ROLE_ORG")
 public class EventDeleteView extends VerticalLayout {
 
-    public EventDeleteView() {
+    @Autowired
+    private CurrentUserDAOInterface currentAccount;
+
+    @PostConstruct
+    public void EventDeleteView() {
         // addClassName("filter-view");
         Button createButton = new Button("Create Event", new Icon(VaadinIcon.PLUS));
         createButton.addClickListener(
                 e -> createButton.getUI().ifPresent(ui -> ui.navigate("create-event")));
-        add(new H1("Events"), createButton);
+
+        Grid<Post> newGrid = new Grid<>();
+        newGrid.setItems(currentAccount.getCurrentAccount().getPosts());
+        newGrid.addComponentColumn(PostComponent::new);
+        add(new H1("Events"), createButton, newGrid);
     }
 }

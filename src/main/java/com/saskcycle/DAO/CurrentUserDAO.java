99685994e2fc5fbac74bb7b2efc5499aa7a956e4
Harris <haf094@usask.ca>
2021-03-26 21:59:44 -0600

    reformatted CurrentUserDAO

--- src/main/java/com/saskcycle/DAO/CurrentUserDAO.java
+++ src/main/java/com/saskcycle/DAO/CurrentUserDAO.java
@@ -12,88 +12,86 @@ import org.springframework.stereotype.Service;
 @Service
 public class CurrentUserDAO implements CurrentUserDAOInterface {
 
-  @Autowired private UserDAOInterface userDAO;
+    @Autowired
+    private UserDAOInterface userDataAccess;
 
-  @Override
-  public Account updateSettings(boolean wantsEmail, boolean wantsText) {
-    UserNotificationSettings settings = new UserNotificationSettings(wantsEmail, wantsText);
-    Account account = getCurrentAccount();
-    account.setNotificationSettings(settings);
-    return userDAO.updateAccount(account);
-  }
+    @Override
+    public Account updateSettings(boolean wantsEmail, boolean wantsText) {
+        UserNotificationSettings settings = new UserNotificationSettings(wantsEmail, wantsText);
+        Account account = getCurrentAccount();
+        account.setNotificationSettings(settings);
+        return userDataAccess.updateAccount(account);
+    }
 
-  @Override
-  public boolean getEmailSetting() {
-    Account account = getCurrentAccount();
-    return account.getNotificationSettings().wantsEmail();
-  }
+    @Override
+    public boolean getEmailSetting() {
+        Account account = getCurrentAccount();
+        return account.getNotificationSettings().wantsEmail();
+    }
 
-  @Override
-  public String getEmail() {
-    Account account = getCurrentAccount();
-    return account.getEmail();
-  }
+    @Override
+    public String getEmail() {
+        Account account = getCurrentAccount();
+        return account.getEmail();
+    }
 
-  @Override
-  public boolean getTextSetting() {
-    Account account = getCurrentAccount();
-    return account.getNotificationSettings().wantsText();
-  }
+    @Override
+    public boolean getTextSetting() {
+        Account account = getCurrentAccount();
+        return account.getNotificationSettings().wantsText();
+    }
 
-  /**
-   * Gets the current user, I.E the user who is currently making a given request
-   *
-   * @return the current user as an Account object
-   */
-  public Account getCurrentAccount() {
-    UserDetails user =
-        (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    return userDAO.searchByName(user.getUsername());
-  }
+    /**
+     * Gets the current user, I.E the user who is currently making a given request
+     *
+     * @return the current user as an Account object
+     */
+    public Account getCurrentAccount() {
+        UserDetails user =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDataAccess.searchByName(user.getUsername());
+    }
 
-  /**
-   * Adds an ID to a logged in user's wishlist
-   * @param id a string representation the ID of a post in the database
-   */
-  public void updateWishlist(String id)
-  {
-    Account account = this.getCurrentAccount();
-    account.getWishlist().add(id);
-    userDAO.updateAccount(account);
-  }
+    /**
+     * Adds an ID to a logged in user's wishlist
+     *
+     * @param id a string representation the ID of a post in the database
+     */
+    public void updateWishlist(String id) {
+        Account account = this.getCurrentAccount();
+        account.getWishlist().add(id);
+        userDataAccess.updateAccount(account);
+    }
 
-  public void removeFromWishlist(String id)
-  {
-    Account account = this.getCurrentAccount();
-    account.getWishlist().remove(id);
-    userDAO.updateAccount(account);
-  }
+    public void removeFromWishlist(String id) {
+        Account account = this.getCurrentAccount();
+        account.getWishlist().remove(id);
+        userDataAccess.updateAccount(account);
+    }
 
-  public void removePost(String id)
-  {
-    Account account = this.getCurrentAccount();
-    account.getPostIds().remove(id);
-    userDAO.updateAccount(account);
-  }
+    public void removePost(String id) {
+        Account account = this.getCurrentAccount();
+        account.getPostIds().remove(id);
+        userDataAccess.updateAccount(account);
+    }
 
-  public void updatePosts(Post p) {
-      Account account = this.getCurrentAccount();
-      account.getPostIds().add(p.id);
-      userDAO.updateAccount(account);
+    public void updatePosts(Post p) {
+        Account account = this.getCurrentAccount();
+        account.getPostIds().add(p.id);
+        userDataAccess.updateAccount(account);
+    }
 
-  }
+    @Override
+    public void deleteEvent(Event saskcycleEvent) {
+        Account account = this.getCurrentAccount();
+        account.getPostIds().remove(saskcycleEvent.getId());
+        userDataAccess.updateAccount(account);
 
-  @Override
-  public void deleteEvent(Event saskcycleEvent) {
-    Account account = this.getCurrentAccount();
-    account.getPostIds().remove(saskcycleEvent.getId());
-    userDAO.updateAccount(account);
+    }
 
-  }
-
-  public void updateCreatedPostList(String id){
-    Account account = this.getCurrentAccount();
-    account.getPostIds().add(id);
-    userDAO.updateAccount(account);
-  }
+    public void updateCreatedPostList(String id) {
+        Account account = this.getCurrentAccount();
+        account.getPostIds().add(id);
+        userDataAccess.updateAccount(account);
+    }
 }

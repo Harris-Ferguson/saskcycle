15f813016a2ae9a5e45109909b16697dc33e4e31
Harris <haf094@usask.ca>
2021-03-27 14:19:18 -0600

    extracted several methods from the map view

--- src/main/java/com/saskcycle/saskcycle/view/uiViews/MapView.java
+++ src/main/java/com/saskcycle/saskcycle/view/uiViews/MapView.java
@@ -34,46 +34,17 @@ public class MapView extends VerticalLayout implements HasUrlParameter<String>, 
         this.setWidth("100%");
 
         // Search bar, transportation selection, and corresponding submit button
-        HorizontalLayout startingAddress = new HorizontalLayout();
-
-        Label startLabel = new Label("Starting Address: ");
-
-        TextField text = new TextField();
-
-        text.setPlaceholder("Input your starting address (EG 123 4th street East)");
-        text.setWidth("500px");
-        text.setId("text");
-
-        Button submitStart = new Button("Get Route");
-        submitStart.setId("submitStart");
-        submitStart.setWidth("150px");
-        startingAddress.add(startLabel, text, submitStart);
-        startingAddress.setAlignItems(Alignment.CENTER);
+        HorizontalLayout startingAddress = getStartingAddress();
 
         // Selection for method of transportation (default walking)
         HorizontalLayout transSelectLayout = new HorizontalLayout();
-        Label transLabel = new Label("Method of Transportation: ");
-
-        Select<String> transSelect = new Select<>("Walking", "Cycling", "Transit", "Driving");
-        transSelect.setValue("Walking");
-        transSelect.setLabel("Method of Transportation:");
-        transSelect.setId("trans");
+        Select<String> transSelect = createTransportationSelector();
 
         // Time selection widget
         HorizontalLayout timeLayout = new HorizontalLayout();
 
-        RadioButtonGroup<String> timeButtons = new RadioButtonGroup<>();
-        timeButtons.setId("timeButton");
-        timeButtons.setItems("Now", "Custom Time");
-        timeButtons.setValue("Now");
-        timeButtons.setLabel("Departure Time:");
-
-        DateTimePicker dtp = new DateTimePicker();
-        dtp.setId("timePick");
-        dtp.setDatePlaceholder("Date");
-        dtp.setTimePlaceholder("Time");
-        dtp.setStep(Duration.ofMinutes(15));
-        dtp.setReadOnly(true);
+        RadioButtonGroup<String> timeButtons = createTimeButtons();
+        DateTimePicker dtp = createDateTimePicker();
 
         // Radio button selection for time Picker clears and disallows custom departure entry when set to "Now"
         timeButtons.addValueChangeListener(event -> {
@@ -106,6 +77,57 @@ public class MapView extends VerticalLayout implements HasUrlParameter<String>, 
         targetAddress.add(dataLabel, pCode, warning);
 
         add(startingAddress, transSelectLayout, map, targetAddress);
+    }
+
+    private Select<String> createTransportationSelector() {
+        Select<String> transSelect = new Select<>("Walking", "Cycling", "Transit", "Driving");
+        transSelect.setValue("Walking");
+        transSelect.setLabel("Method of Transportation:");
+        transSelect.setId("trans");
+        return transSelect;
+    }
+
+    private HorizontalLayout getStartingAddress() {
+        HorizontalLayout startingAddress = new HorizontalLayout();
+
+        Label startLabel = new Label("Starting Address: ");
+
+        TextField text = createPlaceHolderText();
+
+        Button submitStart = new Button("Get Route");
+        submitStart.setId("submitStart");
+        submitStart.setWidth("150px");
+        startingAddress.add(startLabel, text, submitStart);
+        startingAddress.setAlignItems(Alignment.CENTER);
+        return startingAddress;
+    }
+
+    private TextField createPlaceHolderText() {
+        TextField text = new TextField();
+
+        text.setPlaceholder("Input your starting address (EG 123 4th street East)");
+        text.setWidth("500px");
+        text.setId("text");
+        return text;
+    }
+
+    private DateTimePicker createDateTimePicker() {
+        DateTimePicker dtp = new DateTimePicker();
+        dtp.setId("timePick");
+        dtp.setDatePlaceholder("Date");
+        dtp.setTimePlaceholder("Time");
+        dtp.setStep(Duration.ofMinutes(15));
+        dtp.setReadOnly(true);
+        return dtp;
+    }
+
+    private RadioButtonGroup<String> createTimeButtons() {
+        RadioButtonGroup<String> timeButtons = new RadioButtonGroup<>();
+        timeButtons.setId("timeButton");
+        timeButtons.setItems("Now", "Custom Time");
+        timeButtons.setValue("Now");
+        timeButtons.setLabel("Departure Time:");
+        return timeButtons;
     }
 
     @Override
